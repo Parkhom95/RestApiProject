105b0bf825862a22ae8c38532947127fe9b84cfd
Parkhom95 <simbian19@gmail.com>
2025-04-13 20:16:00 +0500

    1

---
+++ app/Models/Product.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\ProductStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Product extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'count',
+        'price',
+        'status',
+    ];
+
+    protected $casts = [
+      'status' => ProductStatus::class,
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function reviews(): HasMany
+    {
+        return $this->hasMany(ProductReview::class);
+    }
+
+    public function image(): HasMany
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+}
